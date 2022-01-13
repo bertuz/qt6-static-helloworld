d11c2759ae7b4af9cb128f8a972c59031b70a89a
matteo.bertamini <matteo.bertamini@telefonica.com>
2022-01-13 22:57:04 +0100

    connect signal to exit

--- configure.sh
+++ configure.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+export CMAKE_PREFIX_PATH=/Users/matteo/.conan/data/qtdeclarative/6.2.2/qt/everywhere/package/d460084946e13a2e2bd394e9438019159b9b7507:/Users/matteo/.conan/data/qtsvg/6.2.2/qt/everywhere/package/3ae3af77b5cdde89b25cbdaab8903af46f1df790:/Users/matteo/.conan/data/qtshadertools/6.2.2/qt/everywhere/package/3ae3af77b5cdde89b25cbdaab8903af46f1df790:/Users/matteo/.conan/data/qtbase/6.2.2/qt/everywhere/package/2b03afe371e4c5cfd9436080f6a933311ea6faef
 mkdir -p build && cd build && conan install ../ --build=missing && cmake -S ../ -B ./
--- src/MyWindow.cpp
+++ src/MyWindow.cpp
@@ -4,6 +4,7 @@
 
 #include "MyWindow.h"
 #include <iostream>
+#include <QApplication>
 
 MyWindow::MyWindow(QWidget *parent) :
         QWidget(parent) {
@@ -12,9 +13,9 @@ MyWindow::MyWindow(QWidget *parent) :
     setFixedSize(200, 100);
 
     // Create and position the button
-    m_button = new QPushButton("Hello World", this);
+    m_button = new QPushButton("Exit", this);
     m_button->setGeometry(10, 10, 80, 30);
-
+    connect(m_button, SIGNAL (clicked()), QApplication::instance(), SLOT (quit()));
 
     std::cout << "Executing my window! \n";
 }
